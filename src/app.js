d47bef60d720ee26861613ef71c7c37cb9f7e1b1
Millad <Milladayoubi@gmail.com>
2020-11-24 15:58:44 -0500

    Directory Creation

--- src/app.js
+++ src/app.js
@@ -1,30 +1,44 @@
 
 const express = require('express')
+const path = require('path')
 
 
+
+console.log(__dirname)
+console.log(path.join(__dirname, '../public'))
+
+const publicDir = path.join(__dirname,'../public')
 const app = express()
 
+app.use(express.static(publicDir))
 
-app.get('', (req, res) => {
-    res.send('Hello Express!')
-})
 
 
 app.get('/help', (req,res) => {
-    res.send('<h1>Obi-Wan: Hello There!</h1>')
+    res.send({
+        name: 'Ashoka',
+        Age: 34,
+        
+    })
 })
-
-
 
 app.get('/about', (req,res) => {
-    res.send('<h1>GENERAL KENOBI!!!!</h1>')
+    res.send([{
+        title: "Node"
+    },
+    {
+        title: "Clone"
+    }
+])
 })
-
-
 
 app.get('/weather', (req,res) => {
     res.send('<h1>This is the Weather Tab</h1>')
 })
+
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
 })
+
+
+
